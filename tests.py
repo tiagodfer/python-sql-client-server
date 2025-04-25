5ecdf32c87e295a6a3eaa5a6c7059a637ea94e1e
Tiago Dias Ferreira <tiagodfer@gmail.com>
2025-04-25 20:13:23 -0300

    Delete tests.py

--- tests.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('db/cnpj.db')
-cursor = conn.cursor()
-#cursor.execute("SELECT * FROM socios WHERE nome LIKE ?", ('%Tiago Dias Ferreira%',))
-cursor.execute("SELECT * FROM socios WHERE cpf_cnpj LIKE 000000")
-tables = cursor.fetchall()
-print(tables)
-conn.close()
